c60ee4cd512f3108bf478aa19117768f1df2de6d
Steve White <swhite22@student.waubonsee.edu>
2019-03-05 11:50:12 -0600

    added all files

---
+++ pass.py
@@ -0,0 +1,8 @@
+correct_pass = "meme"
+
+password = input(print('Please enter password: '))
+
+if password  correct_pass:
+    print('correct password entered, logging in...')
+else:
+    print('incorrect password')
