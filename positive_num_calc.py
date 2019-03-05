50121c9e111dfe6912961a1519f7194ee681e798
Steve White <swhite22@student.waubonsee.edu>
2019-03-05 12:23:09 -0600

    positive_num_calc.py

--- positive_num_calc.py
+++ positive_num_calc.py
@@ -5,3 +5,8 @@ sum_add = positive_num1 + positive_num2
 sum_sub = positive_num1 - positive_num2
 
 print('the sum of the numbers are: ', sum_add, ', and the difference is: ', sum_sub)
+
+sum_mult = positive_num1 * positive_num2
+sum_div = positive_num1 / positive_num2
+
+print('the product is: ', sum_mult,'the quotient is:', sum_div)
