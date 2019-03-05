cfd1b534e9300a1f57e30f0e3e7ed312bfe9884a
Steve White <swhite22@student.waubonsee.edu>
2019-03-05 12:19:28 -0600

    positive_num_calc.py

--- positive_num_calc.py
+++ positive_num_calc.py
@@ -1,4 +1,7 @@
-positive_num1 = 0
-positive_num2 = 0
+positive_num1 = float(input('input positive number'))
+positive_num2 = float(input('input positive number'))
 
-positive_num1 = input(print"Input positive number"))
+sum_add = positive_num1 + positive_num2
+sum_sub = positive_num1 - positive_num2
+
+print('the sum of the numbers are: ', sum_add, ', and the difference is: ', sum_sub)
