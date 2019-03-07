3a19a5b1eb8a12b433737146cbd1d90dd8433505
Steve White <swhite22@student.waubonsee.edu>
2019-03-07 11:43:14 -0600

    added a loop until the correct password is entered

--- password.py
+++ password.py
@@ -5,4 +5,10 @@ password = input('Please enter password: ')
 if password == correct_pass:
     print('correct password entered, logging in...')
 else:
-    print('incorrect password')
+    while password != correct_pass:
+        
+        password = input('incorrect password, please try again: ')
+        
+        if password == correct_pass:
+            print('correct password entered, logging in...')
+        
